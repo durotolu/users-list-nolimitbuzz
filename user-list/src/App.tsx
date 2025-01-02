f8707f88754170285c3c8d635a917e5281ef9499
durotolu <durolawk@gmail.com>
2025-01-02 23:06:25 +0100

    updated app

--- user-list/src/App.tsx
+++ user-list/src/App.tsx
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import UserDetailsPage from './pages/UserDetailsPage';
+import Navbar from './components/Navbar';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App: React.FC = () => (
+  <Router>
+    <Navbar />
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/user/:id" element={<UserDetailsPage />} />
+    </Routes>
+  </Router>
+);
 
 export default App;
